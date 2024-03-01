e6eafaf8b4644e0b8c9b4b4d6dff8030d5b9cbd2
govindagoud <govindagoud.patil@hotmail.com>
2024-03-01 12:40:12 -0800

    added kiota and modified pages to use

--- Movies_BlazorWASM/Program.cs
+++ Movies_BlazorWASM/Program.cs
@@ -1,6 +1,10 @@
+using KiotaMovies.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Kiota.Abstractions;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
 using Movies_BlazorWASM;
 using Movies_BlazorWASM.Data;
 
@@ -11,7 +15,15 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddDbContext<MoviesDbContext>(options =>
     options.UseInMemoryDatabase("Movies"));
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped((sp) =>
+{
+    var authprovider = new AnonymousAuthenticationProvider();
+    var adapter = new HttpClientRequestAdapter(authprovider,httpClient:new HttpClient() { BaseAddress = new Uri("https://localhost:7254/") });
+    return new MoviesClient(adapter);
+
+});
+
+
 
 builder.Services.AddOidcAuthentication(options =>
 {
